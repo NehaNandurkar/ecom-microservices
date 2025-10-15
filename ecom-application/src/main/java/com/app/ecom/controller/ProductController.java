74a44f0b135125321350a5a07dc44fd6f982fd83
NehaNandurkar <nandurkarneha12@gmail.com>
2025-10-15 07:03:22 +0530

    Committing code changes for ProductController

--- ecom-application/src/main/java/com/app/ecom/controller/ProductController.java
+++ ecom-application/src/main/java/com/app/ecom/controller/ProductController.java
@@ -1,15 +1,19 @@
 package com.app.ecom.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.ecom.dto.ProductRequest;
@@ -28,6 +32,12 @@ public class ProductController  {
 		return new ResponseEntity<ProductResponse>(productService.createProduct(productRequest),HttpStatus.CREATED);
 	}
 	
+	
+	@GetMapping
+	public ResponseEntity<List<ProductResponse>> getProducts() {
+	    return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+	}
+	
 	@PutMapping("/{id}")
 	public ResponseEntity<ProductResponse> updateProduct(@RequestBody ProductRequest productRequest, @PathVariable Long id){
 		Optional<ProductResponse> updatedProduct = productService.updateProduct(productRequest, id);
@@ -38,5 +48,24 @@ public class ProductController  {
 	        return new ResponseEntity<>(HttpStatus.NOT_FOUND); 
 	    }
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+	    boolean deleted=productService.deleteProduct(id);
+	    if(deleted) {
+	    	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    }else {
+	    	return new ResponseEntity<>(HttpStatus.NOT_FOUND); 
+	    }
+	    
+	}
+	
+	@GetMapping("/search")
+	public ResponseEntity<List<ProductResponse>> searchProduct(@RequestParam String keyword) {
+	    return new ResponseEntity<>(productService.searchProducts(keyword),HttpStatus.OK);
+	    
+	}
+
+	
 
 }
--- ecom-application/src/main/java/com/app/ecom/service/ProductService.java
+++ ecom-application/src/main/java/com/app/ecom/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.app.ecom.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,6 @@ import org.springframework.stereotype.Service;
 import com.app.ecom.dto.ProductRequest;
 import com.app.ecom.dto.ProductResponse;
 import com.app.ecom.model.Product;
-import com.app.ecom.model.User;
 import com.app.ecom.repository.ProductRepository;
 
 @Service
@@ -24,6 +25,60 @@ public class ProductService {
 		
 		return mapToProductResponse(savedProduct);
 	}
+	
+	public Optional<ProductResponse> updateProduct(ProductRequest productRequest, Long id) {
+		Optional<Product> optionalProduct = productRepository.findById(id);
+	    
+	    if (optionalProduct.isPresent()) {
+	    	Product product = optionalProduct.get();
+	        updateProductFromRequest(product,productRequest);
+	        Product savedProduct = productRepository.save(product);
+	        return Optional.of(mapToProductResponse(savedProduct));  
+	    }
+		return Optional.empty();
+	}
+	
+	public List<ProductResponse> getAllProducts() {
+	    List<Product> products = productRepository.findByActiveTrue();
+	    List<ProductResponse> productResponses = new ArrayList<>();
+
+	    for (Product product : products) {
+	        ProductResponse response = mapToProductResponse(product);
+	        productResponses.add(response);
+	    }
+
+	    return productResponses;
+	}
+
+	public boolean deleteProduct(Long id) {
+	    Optional<Product> optionalProduct = productRepository.findById(id);
+
+	    if (optionalProduct.isPresent()) {
+	        Product product = optionalProduct.get();
+
+	        if (Boolean.TRUE.equals(product.getActive())) {
+	            product.setActive(false); 
+	            productRepository.save(product);
+	            return true; 
+	        } else {
+	            return false; 
+	        }
+	    } else {
+	        return false; 
+	    }
+	}
+
+	public List<ProductResponse> searchProducts(String keyword) {
+		List<Product> products = productRepository.searchProducts(keyword);
+		List<ProductResponse> productResponses=new ArrayList<>();
+		for(Product product:products) {
+			 ProductResponse response = mapToProductResponse(product);
+		     productResponses.add(response);
+		}
+		return productResponses;
+	}
+
+
 
 	private ProductResponse mapToProductResponse(Product savedProduct) {
 		ProductResponse productResponse= new ProductResponse();
@@ -48,16 +103,14 @@ public class ProductService {
 		
 	}
 
-	public Optional<ProductResponse> updateProduct(ProductRequest productRequest, Long id) {
-		Optional<Product> optionalProduct = productRepository.findById(id);
-	    
-	    if (optionalProduct.isPresent()) {
-	    	Product product = optionalProduct.get();
-	        updateProductFromRequest(product,productRequest);
-	        Product savedProduct = productRepository.save(product);
-	        return Optional.of(mapToProductResponse(savedProduct));  
-	    }
-		return Optional.empty();
-	}
+
+
+
+
+
+
+
+
+
 
 }
