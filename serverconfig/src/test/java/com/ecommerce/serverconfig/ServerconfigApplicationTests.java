f30ccdd451eec6348153b9df4aaa44dd1ab1f183
NehaNandurkar <nandurkarneha12@gmail.com>
2025-11-07 07:04:44 +0530

    Committing code changes for config server

--- serverconfig/src/test/java/com/ecommerce/serverconfig/ServerconfigApplicationTests.java
+++ serverconfig/src/test/java/com/ecommerce/serverconfig/ServerconfigApplicationTests.java
@@ -1,10 +1,10 @@
-package com.demo.configserver;
+package com.ecommerce.serverconfig;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigserverApplicationTests {
+class ServerconfigApplicationTests {
 
 	@Test
 	void contextLoads() {
