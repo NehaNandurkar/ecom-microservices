c74b324cd48368ab5a765090b6780c3ba7cc16cf
NehaNandurkar <nandurkarneha12@gmail.com>
2025-11-09 07:50:29 +0530

    Code changes for  Inter service communication using RestTemplate

---
+++ interservice/consumer/src/main/java/com/demo/consumer/resttemplate/RestTemplateClient.java
@@ -0,0 +1,19 @@
+package com.demo.consumer.resttemplate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestTemplateClient {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private static final String PROVIDER_URL="http://localhost:8081";
+	
+	public String getInstanceInfo() {
+		return restTemplate.getForObject(PROVIDER_URL+"/instance-info",String.class);
+	}
+
+}
