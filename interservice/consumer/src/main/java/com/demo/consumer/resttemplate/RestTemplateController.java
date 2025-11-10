cd5e3d95ab33713df7c0f0591da24b4e2288dc3e
NehaNandurkar <nandurkarneha12@gmail.com>
2025-11-10 06:17:01 +0530

    InterService communication using Feign Client

--- interservice/consumer/src/main/java/com/demo/consumer/resttemplate/RestTemplateController.java
+++ interservice/consumer/src/main/java/com/demo/consumer/resttemplate/RestTemplateController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping("/api/rest-template")
