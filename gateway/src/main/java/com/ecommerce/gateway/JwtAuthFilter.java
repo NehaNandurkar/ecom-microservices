89e1e6ed43bd150f7a5b87ac54f0653703f7c47a
NehaNandurkar <nandurkarneha12@gmail.com>
2025-11-19 07:03:21 +0530

    Changes for JWT logging

---
+++ gateway/src/main/java/com/ecommerce/gateway/JwtAuthFilter.java
@@ -0,0 +1,25 @@
+package com.ecommerce.gateway;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+
+import reactor.core.publisher.Mono;
+@Component
+public class JwtAuthFilter implements WebFilter{
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		String authHeaders=exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+		if(authHeaders==null || !authHeaders.startsWith("Bearer ")) {
+			exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+			return exchange.getResponse().setComplete();
+		}
+		return chain.filter(exchange);
+	}
+
+}
